62692eb3357e58593365ff7ce6317c3585b55645
Drew F. Sousa <drew@wracked.com>
2022-09-07 15:21:41 -0700

    Initial Commit
    Simple script to callback webURL to capture remote host IP

---
+++ Powershell_report_back_IP.ps1
@@ -0,0 +1,7 @@
+﻿# DFS
+# Report IP back to URL
+
+$HOSTNAME = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
+$CALLBACKURL = $("https://wracked.com/search.html?code=" + "$HOSTNAME")
+$CURL = New-Object System.Net.WebClient
+$CURL.DownloadString("$CALLBACKURL") | Out-Null

4648ba103915bf440e4e82c61dfb0a1116b9cfbc
Drew F. Sousa <drew@wracked.com>
2022-09-28 17:23:08 -0700

    Put in generic CALLBACKURL

--- Powershell_report_back_IP.ps1
+++ Powershell_report_back_IP.ps1
@@ -1,7 +1,7 @@
-﻿# DFS
+# DFS
 # Report IP back to URL
 
 $HOSTNAME = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
-$CALLBACKURL = $("https://wracked.com/search.html?code=" + "$HOSTNAME")
+$CALLBACKURL = $("https://yourURL.com/search.html?code=" + "$HOSTNAME")
 $CURL = New-Object System.Net.WebClient
 $CURL.DownloadString("$CALLBACKURL") | Out-Null

05262db675a5ee0033c6d03bbbb48cc9262a2dd6
Drew Sousa <drew@wracked.com>
2023-01-17 17:52:46 -0800

    Updated description

--- Powershell_report_back_IP.ps1
+++ Powershell_report_back_IP.ps1
@@ -1,5 +1,6 @@
-# DFS
-# Report IP back to URL
+# Drew F. Sousa
+# Description: Report IP back to URL
+# Sometimes debugging X-Forward or running something like #>tail -f \var\log\apache_httd.log you need to make some noise.
 
 $HOSTNAME = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
 $CALLBACKURL = $("https://yourURL.com/search.html?code=" + "$HOSTNAME")
